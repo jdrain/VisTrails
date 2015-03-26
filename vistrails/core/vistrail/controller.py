1264fd6b6117062f78a34675cf5727dfb4fedab3
Remi Rampin <remirampin@gmail.com>
2015-03-26 16:59:12 -0400

    Removes all kind of useless properties from core

--- vistrails/core/vistrail/controller.py
+++ vistrails/core/vistrail/controller.py
@@ -197,19 +197,6 @@ class VistrailController(object):
                           id_scope=id_scope, 
                           set_log_on_vt=set_log_on_vt)
 
-    # allow gui.vistrail_controller to reference individual views
-    def _get_current_version(self):
-        return self._current_version
-    def _set_current_version(self, version):
-        self._current_version = version
-    current_version = property(_get_current_version, _set_current_version)
-
-    def _get_current_pipeline(self):
-        return self._current_pipeline
-    def _set_current_pipeline(self, pipeline):
-        self._current_pipeline = pipeline
-    current_pipeline = property(_get_current_pipeline, _set_current_pipeline)
-
     def flush_pipeline_cache(self):
         self._pipelines = {0: Pipeline()}
 
